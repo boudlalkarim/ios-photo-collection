b20b657b87216b0a371565ab85c7ce7015911377
Karim Boudlal <karim_boudlal@yahoo.com>
2019-10-10 23:24:00 -0400

    started1

---
+++ PhotoCollection/PhotoCollection/Photo.swift
@@ -0,0 +1,13 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by Karim Boudlal on 10/10/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+struct Photo {
+    var imageData: Data
+    var title: String
+}
